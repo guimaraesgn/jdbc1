317c5b980e9a46308519058b537306ee23ab0e5c
unknown <guimaraesgn30@gmail.com>
2023-03-02 22:36:23 -0300

    transação no jdbc

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 	
@@ -13,31 +17,38 @@ public class Program {
 		
 		Connection conn = null;
 		Statement st = null;
-		ResultSet rs = null;
-		
 		try {
 			conn = DB.getConnection();
 			
+			conn.setAutoCommit(false);
 			
 			st = conn.createStatement();
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1" );
 			
-			rs = st.executeQuery("select * from department");
+			//int x = 1;
+			//if (x < 2) {
+			//	throw new SQLException("fAKE ERROR" );
+			//}
 			
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2" );
+			
+			conn.commit();
+
+			System.out.println("rows1 "+ rows1);
+			System.out.println("rows2 "+ rows2);
+		}
+		catch (SQLException e) {
+			try {
+				conn.rollback();
+				throw new DbException("TRANSACTION ROLLED back! caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to rollback CAUSED BY: " + e1.getMessage());
 			}
 		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
 		finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
-			DB.closeConnection();
+			DB.getConnection();
 		}
-		
-		
-		
 		
 	}
 
